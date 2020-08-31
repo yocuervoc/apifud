62657ef6ffae137a680000d2c6f2b7bf78edc5f9
yocuervoc <yocuervoc@unal.edu.co>
2020-08-31 02:47:56 -0500

    dbModelRefactoring

--- model/warehouse.js
+++ model/warehouse.js
@@ -6,34 +6,34 @@ const setupDatabase = require('../db')
 module.exports = function setupWarehouseModel (config){
     const sequelize = setupDatabase (config)
 
-    return sequelize.define('warehouse', {
-        id: {
-            type: Sequelize.INTEGER.UNSIGNED,
+    return sequelize.define('Warehouse', {
+        warehouse_id: {
+            type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true,
             allowNull: false
         },
         name: {
-            type: Sequelize.STRING(50),
+            type: Sequelize.STRING(20),
             allowNull: false
         },
         headquarters_number: {
-            type: Sequelize.INTEGER,
+            type: Sequelize.STRING(20),
             allowNull: false
         },
         created_at: {
-            type: Sequelize.DATE(6),
-            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(6)'),
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
             allowNull: true
         },
         updated_at: {
-            type: Sequelize.DATE(6),
-          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(6)'),
+            type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
           allowNull: true
         }
     }, {
         timestamps: false,
         underscored: true,
-        tableName: 'warehouse'
+        tableName: 'Warehouse'
       })
 }
--- model/warehouseDescription.js
+++ model/warehouseDescription.js
@@ -6,42 +6,42 @@ const setupDatabase = require('../db')
 module.exports = function setupwarehouseDescriptionModel (config){
     const sequelize = setupDatabase (config)
 
-    return sequelize.define('warehouse_description', {
+    return sequelize.define('Warehouse_description', {
         id: {
-            type: Sequelize.INTEGER.UNSIGNED,
+            type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true,
             allowNull: false
         },
-        warehouse_id: {
-            type: Sequelize.INTEGER.UNSIGNED,
-            allowNull: false
-        },
-        phone: {
+        fk_warehouse_id: {
             type: Sequelize.INTEGER,
             allowNull: false
         },
+        phone: {
+            type: Sequelize.STRING(20),
+            allowNull: false
+        },
         address: {
-            type: Sequelize.STRING(150),
+            type: Sequelize.STRING(20),
             allowNull: false
         },
         city: {
-            type: Sequelize.STRING(150),
+            type: Sequelize.STRING(20),
             allowNull: false
         },
         created_at: {
-            type: Sequelize.DATE(6),
-            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(6)'),
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
             allowNull: true
         },
         updated_at: {
-            type: Sequelize.DATE(6),
-          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(6)'),
+            type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
           allowNull: true
         }
     }, {
         timestamps: false,
         underscored: true,
-        tableName: 'warehouse_description'
+        tableName: 'Warehouse_description'
       })
 }
